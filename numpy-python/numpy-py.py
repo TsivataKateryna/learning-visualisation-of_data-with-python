04b57b989460e111405c7bf597c9184794bd58ae
KaterynaTsivata <kateryna.tsivata@gmail.com>
2025-09-10 14:11:37 +0200

    working with numpy

---
+++ numpy-python/numpy-py.py
@@ -0,0 +1,15 @@
+import numpy as nm
+import random as rd
+
+def matrix_analysis():
+    pass
+
+def interpolation():
+    pass
+
+def mini_calculator():
+    pass
+
+print(matrix_analysis())
+print(interpolation())
+print(mini_calculator())
