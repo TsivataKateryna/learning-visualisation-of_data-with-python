ccb0fb52a710aca4bdb5bcd8185dd578add91c0c
KaterynaTsivata <kateryna.tsivata@gmail.com>
2025-09-10 16:08:56 +0200

    practicing numpy

---
+++ numpy-python/numpy-3Dmatrix-py.py
@@ -0,0 +1,12 @@
+import numpy as np
+import random as rd
+
+def cube_statistics(matrix : np.ndarray) -> None:
+    pass
+
+def function():
+    pass
+
+data = np.random.randint(0, 100, size=(4,5,6))
+print(cube_statistics(data))
+print()
--- numpy-python/numpy-py.py
+++ numpy-python/numpy-py.py
@@ -1,8 +1,14 @@
-import numpy as nm
+import numpy as np
 import random as rd
 
-def matrix_analysis():
-    pass
+def matrix_analysis(matrix : np.ndarray) -> None:
+    print("matrix : ", matrix)
+    print("determinant = ", np.linalg.det(matrix))
+    print("rank :", np.linalg.matrix_rank(matrix))
+    print("trace :", np.trace(matrix))
+    print("inverse :", np.linalg.inv(matrix))
+    print("norm :",  np.linalg.norm(matrix))
+    print("eigenvalues / eigenvectors : ", np.linalg.eig(matrix))
 
 def interpolation():
     pass
@@ -10,6 +16,9 @@ def interpolation():
 def mini_calculator():
     pass
 
-print(matrix_analysis())
-print(interpolation())
-print(mini_calculator())
+
+A = np.random.randint(0, 10, size=(3, 3))
+
+print(matrix_analysis(A))
+# print(interpolation())
+# print(mini_calculator())
