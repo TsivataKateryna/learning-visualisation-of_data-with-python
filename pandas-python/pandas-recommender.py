d9ca1060cd09fd5caf38086d073dffc613e76f7c
KaterynaTsivata <kateryna.tsivata@gmail.com>
2025-09-10 21:09:03 +0200

    working on pandas-recommender

--- pandas-python/pandas-recommender.py
+++ pandas-python/pandas-recommender.py
@@ -1,9 +1,21 @@
 import pandas as pd
 import numpy as np
+import os
 
-possible_choice = ['', '', '']
+# possible_choice = ['', '', '']
+
+def to_parquet(df) -> None :
+    df.to_parquet("bios_new.parquet", engine='pyarrow', index=False)
 
 
 def chose():
     pass
 
+
+
+
+df = pd.read_csv('./my_data_kaggle/IMDBTop250Movies.csv')
+# print(df.type)
+print(os.path.exists('data/IMDBTop250Movies.csv'))
+print(os.getcwd())  
+print(os.listdir()) 
---
+++ python/lambda-fun.py
@@ -0,0 +1,13 @@
+import math as mt
+
+'learning lambda-function'
+
+square = lambda x: x**2
+print(square(5)) 
+
+add = lambda a, b: a + b
+print(add(3, 7))
+
+numbers = [1, 2, 3, 4]
+squared_numbers = list(map(lambda x: x**2, numbers))
+print(squared_numbers)
