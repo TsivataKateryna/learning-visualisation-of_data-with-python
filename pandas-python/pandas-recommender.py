2a1d27ee1e1e76b538602e9572f922c1a84f9ef9
KaterynaTsivata <kateryna.tsivata@gmail.com>
2025-09-11 18:59:02 +0200

    working on pandas-recommender and statistics

--- pandas-python/pandas-recommender.py
+++ pandas-python/pandas-recommender.py
@@ -2,20 +2,65 @@ import os
 import numpy as np
 import pandas as pd
 
-# possible_choice = ['', '', '']
-def stat_box_office() -> None:
-    pass
+def convert_time_int(str_time : str) -> int:
+    hours : int = 0
+    min : int = 0
 
+    if 'h' in duration:
+        hours_part = duration.split('h')[0]
+        hours = int(hours_part.strip())
+        duration = duration.split('h')[1] 
+    
+    if 'm' in duration:
+        minutes_part = duration.split('m')[0]
+        minutes = int(minutes_part.strip())
+    
+    return hours * 60 + minutes
+
+def stat_box_office() -> None:
+    print(df['box_office'].values)
+
+''
 def stat_year() -> None:
     the_earliest_year : int = df['year'].min()
-    the_earliest_film = df.loc[df['year'] == the_earliest_year, ['name', 'year']]
-    print("the_earliest_film ", the_earliest_film)
+    the_earliest_film = df.loc[df['year'] == the_earliest_year]
+    # print(" sidoiervner", type(the_earliest_film))
+    print("the_earliest_film \n", the_earliest_film)
+
+    the_oldest_year : int = df['year'].max()
+    the_oldest_film = df.loc[df['year'] == the_oldest_year]
+    print("the_oldest_film \n", the_oldest_film)
+    
+    print("Number of films per year")
+    year_counts = df['year'].value_counts()
+    year_counts_sorted = year_counts.sort_values(ascending=False)
+    print(year_counts_sorted.head(5))
+
+def stat_certificate() -> None:
+    pass
+
+def stat_run_time() -> None:
+    print(df['run_time'].values)
+
+def new_data() -> None:
+    new_data = df.copy()
+    new_data.to_parquet("IMDBTop250Movies_new.parquet", engine='pyarrow', index=False)
+    print(new_data.info())
+
+
+def stat_tagline() -> None:
+    # print(df['tagline'].values)
     pass
 
 def stat_rating() -> None:
     mean_rating : float = df['rating'].mean()
-    print("here ", mean_rating)
-    pass
+    print("mean_rating ", mean_rating)
+    median_rating : int = df['rating'].median()
+    print("median_rating ", median_rating)
+
+def stat_budget() -> None:
+    print(df['budget'].values)
+
 
 def to_prepare_statistic() -> None:
     df['directors'] = df['directors'].str.split(',')
@@ -43,12 +88,18 @@ def chose(filename):
 
 filename = 'IMDBTop250Movies.csv'
 df = pd.read_csv(filename)
-print(df.info())
+# print(df.info())
+
+# stat_rating()
+# stat_tagline()
+# stat_run_time()
+# stat_budget()
+# new_data()
 
 # print(df.head())
 # print(df['rating'])
 # stat_rating()
-stat_year()
+# stat_year()
 # print(df['certificate'])
 # print(df['box_office'].values)
 # print(df['directors'].values)
@@ -62,3 +113,5 @@ stat_year()
 # print(df.columns)
 # print(type(df)) # <class 'pandas.core.frame.DataFrame'>
 # movie_statistics()
+
+# stat_box_office()
