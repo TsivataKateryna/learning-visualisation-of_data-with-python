3eefda90355bf743ae7e036f754701a1ac590521
KaterynaTsivata <kateryna.tsivata@gmail.com>
2025-09-11 18:21:41 +0200

    working on pandas-recommender

--- pandas-python/pandas-recommender.py
+++ pandas-python/pandas-recommender.py
@@ -1,8 +1,24 @@
-import pandas as pd
-import numpy as np
 import os
+import numpy as np
+import pandas as pd
 
 # possible_choice = ['', '', '']
+def stat_box_office() -> None:
+    pass
+
+def stat_year() -> None:
+    the_earliest_year : int = df['year'].min()
+    the_earliest_film = df.loc[df['year'] == the_earliest_year, ['name', 'year']]
+    print("the_earliest_film ", the_earliest_film)
+    pass
+
+def stat_rating() -> None:
+    mean_rating : float = df['rating'].mean()
+    print("here ", mean_rating)
+    pass
+
+def to_prepare_statistic() -> None:
+    df['directors'] = df['directors'].str.split(',')
 
 def to_parquet(filename : str) -> None :
     if filename.lower().endswith('.csv'):
@@ -10,6 +26,10 @@ def to_parquet(filename : str) -> None :
 
 def movie_statistics()-> None:
     unique_genre : int = df['genre'].nunique()
+    list_genres = df['genre'].unique()
+    # print(list_genres)
+
+
 
     pass
 
@@ -23,11 +43,22 @@ def chose(filename):
 
 filename = 'IMDBTop250Movies.csv'
 df = pd.read_csv(filename)
-# print(df.info())
+print(df.info())
+
+# print(df.head())
+# print(df['rating'])
+# stat_rating()
+stat_year()
+# print(df['certificate'])
+# print(df['box_office'].values)
+# print(df['directors'].values)
+# to_prepare_statistic()
+# print(df['directors'].values)
+# print(df['directors'].nunique())
 # print(df['genre'].unique())
 # print(df['genre'].nunique())
 
 # print(df[['name','year','genre']].head())
 # print(df.columns)
-print(type(df)) # <class 'pandas.core.frame.DataFrame'>
-
+# print(type(df)) # <class 'pandas.core.frame.DataFrame'>
+# movie_statistics()
--- python/lambda-fun.py
+++ python/lambda-fun.py
@@ -3,10 +3,31 @@ import math as mt
 'learning lambda-function'
 
 square = lambda x: x**2
-print(square(5)) 
+# print(square(5)) 
 
 add = lambda a, b: a + b
-print(add(3, 7))
+# print(add(3, 7))
+
+'Calculating the factorial of a number (using recursion and the ternary operator).'
+# factorial = lambda a: 
+
+''' 
+Determining whether a number is even or odd.
+return : 
+
+'''
+isEven = lambda a: True if a % 2 == 0 else False 
+# print(isEven(1)) # false
+# print(isEven(2)) # true
+# print(isEven(0)) # true
+# print(isEven(-3)) # false
+# print(isEven(1000000000)) # true
+# print(isEven(100000000000000001)) # false
+
+'Calculating the sum of the squares of the elements in a list.'
+
+
+'Calculating the arithmetic mean of a list of numbers.'
 
 
 
@@ -15,4 +36,4 @@ print(add(3, 7))
 
 numbers = [1, 2, 3, 4]
 squared_numbers = list(map(lambda x: x**2, numbers))
-print(squared_numbers)
+# print(squared_numbers)
