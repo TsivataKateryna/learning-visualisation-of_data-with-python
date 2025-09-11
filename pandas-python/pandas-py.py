34eb2104527a3a0132f0a853a1dfd7db61d5f748
KaterynaTsivata <kateryna.tsivata@gmail.com>
2025-09-11 13:38:10 +0200

    update

--- pandas-python/pandas-py.py
+++ pandas-python/pandas-py.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 bios = pd.read_csv('https://raw.githubusercontent.com/KeithGalli/complete-pandas-tutorial/refs/heads/master/data/bios.csv')
 
-height_input = float(input(" ennter the minimum of hight"))
+height_input = float(input(" enter the minimum hight "))
 tall_people = bios.loc[bios["height_cm"] > height_input, ["name", "height_cm"]]
 print(tall_people)
--- pandas-python/pandas-recommender.py
+++ pandas-python/pandas-recommender.py
@@ -8,6 +8,11 @@ def to_parquet(filename : str) -> None :
     if filename.lower().endswith('.csv'):
         df.to_parquet("bios_new.parquet", engine='pyarrow', index=False)
 
+def movie_statistics()-> None:
+    unique_genre : int = df['genre'].nunique()
+
+    pass
+
 def chose(filename):
     df = pd.read_csv(filename)
     to_parquet(filename)
@@ -18,9 +23,11 @@ def chose(filename):
 
 filename = 'IMDBTop250Movies.csv'
 df = pd.read_csv(filename)
-# print(df.info)
-# print(df[['name','year','genre']].head())
 # print(df.info())
+# print(df['genre'].unique())
+# print(df['genre'].nunique())
+
+# print(df[['name','year','genre']].head())
 # print(df.columns)
 print(type(df)) # <class 'pandas.core.frame.DataFrame'>
 
